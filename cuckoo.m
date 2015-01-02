f29402e873a3060a02f4575b70cbe64ab801b45b
Miguel Flores <floresmigu3l@gmail.com>
2015-01-02 01:10:00 -0800

    Functions and getting to know octave

--- cuckoo.m
+++ cuckoo.m
@@ -1,1 +1,12 @@
-log10(100)/log10(10)
+% Cuckoo Search for nonlinear constrained optimization
+
+function [bestoso, val] = cuckoo_search_spring(time)
+  format long;
+    help cuckoo.m
+    if nargin < 1,
+      % Number of iterations
+      time = 2000;
+    end
+    disp('Computing ... it may take a few minutes.');
+    % Number of nests (different solutions)
+    n = 25;
